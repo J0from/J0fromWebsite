e42184647c0d398e70b1ef24e360ebac592f8d78
Jeremy <jeremyyanikknowles@gmail.com>
2026-02-16 05:07:35 -0500

    Slightly larger Get Jo button on enterprise (h-12 px-10 text-lg)

--- components/enterprise/framework-section.tsx
+++ components/enterprise/framework-section.tsx
@@ -51,7 +51,7 @@ export function FrameworkSection() {
 
         <div className="mt-12 text-center">
           <Link href="/solutions/smb">
-            <Button variant="gradient" className="h-12 px-8 text-base">
+            <Button variant="gradient" className="h-12 px-10 text-lg">
               Get Jo
             </Button>
           </Link>
